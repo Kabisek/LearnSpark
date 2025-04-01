b38c7d0ed166ebeef79071a432b0bb811fa1a2cf
Divanka Samarasinghe <119291780+divankase@users.noreply.github.com>
2025-04-02 02:28:59 +0530

    recreate

--- project-backend/learningplatform/src/main/java/com/example/learningplatform/LearningplatformApplication.java
+++ project-backend/learningplatform/src/main/java/com/example/learningplatform/LearningplatformApplication.java
@@ -1,13 +1,13 @@
-package com.example.project_backend;
+package com.example.learningplatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodolistBackendApplication {
+public class LearningplatformApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TodolistBackendApplication.class, args);
+		SpringApplication.run(LearningplatformApplication.class, args);
 	}
 
 }
--- project-backend/learningplatform/src/test/java/com/example/learningplatform/LearningplatformApplicationTests.java
+++ project-backend/learningplatform/src/test/java/com/example/learningplatform/LearningplatformApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.project_backend;
+package com.example.learningplatform;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TodolistBackendApplicationTests {
+class LearningplatformApplicationTests {
 
 	@Test
 	void contextLoads() {
