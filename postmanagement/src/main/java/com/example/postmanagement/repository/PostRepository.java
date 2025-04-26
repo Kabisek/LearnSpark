35e7a2de70c29dfe1739e013ddf91d0bc44e4889
Kabisek <kabiseksundar02@gmail.com>
2025-04-26 20:28:23 +0530

    deleted the folder

--- postmanagement/src/main/java/com/example/postmanagement/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package main.java.com.example.postmanagement.repository;
-
-import com.example.postmanagement.entity.Post;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PostRepository extends MongoRepository<Post, String> {
-}
