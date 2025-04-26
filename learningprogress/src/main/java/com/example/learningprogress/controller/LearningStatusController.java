97522338552c185f4dbac8c0cd6a11db157089aa
Sanj Techai <115185177+SanjayanT@users.noreply.github.com>
2025-04-26 20:13:14 +0530

    LearningStatusController Updated

--- learningprogress/src/main/java/com/example/learningprogress/controller/LearningStatusController.java
+++ learningprogress/src/main/java/com/example/learningprogress/controller/LearningStatusController.java
@@ -1,5 +1,46 @@
 package com.example.learningprogress.controller;
 
+import com.example.learningprogress.entity.LearningStatus;
+import com.example.learningprogress.service.StatusService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/status")
 public class LearningStatusController {
 
+    @Autowired
+    private StatusService service;
+
+    @PostMapping
+    public LearningStatus createStatus(@RequestBody LearningStatus status) {
+        return service.createStatus(status);
+    }
+
+    @GetMapping
+    public List<LearningStatus> getAllActiveStatuses() {
+        return service.getAllActiveStatuses();
+    }
+
+    @GetMapping("/{id}")
+    public LearningStatus getStatusById(@PathVariable String id) {
+        return service.getStatusById(id);
+    }
+
+    @GetMapping("/user/{userId}")
+    public List<LearningStatus> getStatusesByUserId(@PathVariable String userId) {
+        return service.getStatusesByUserId(userId);
+    }
+
+    @PutMapping("/{id}")
+    public LearningStatus updateStatus(@PathVariable String id, @RequestBody LearningStatus status) {
+        return service.updateStatus(id, status);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteStatus(@PathVariable String id) {
+        service.deleteStatus(id);
+    }
 }
