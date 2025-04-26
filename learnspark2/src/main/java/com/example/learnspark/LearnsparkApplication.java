7486954ae495e596f0d5fa9f9e9ea3f1b23296ad
Divanka Samarasinghe <119291780+divankase@users.noreply.github.com>
2025-04-26 17:10:17 +0530

    Project creating

---
+++ learnspark2/src/main/java/com/example/learnspark/LearnsparkApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnspark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnsparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnsparkApplication.class, args);
+	}
+
+}
