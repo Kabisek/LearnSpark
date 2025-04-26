b46a5ce1e1292121204eeec9daa86fc06f3848ae
Kabisek <kabiseksundar02@gmail.com>
2025-04-26 20:02:38 +0530

    Coded the Repository file

--- postmanagement/src/main/java/com/example/postmanagement/repository/PostRepository.java
+++ postmanagement/src/main/java/com/example/postmanagement/repository/PostRepository.java
@@ -1,3 +1,7 @@
-public class PostRepository {
+package main.java.com.example.postmanagement.repository;
 
+import com.example.postmanagement.entity.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface PostRepository extends MongoRepository<Post, String> {
 }
