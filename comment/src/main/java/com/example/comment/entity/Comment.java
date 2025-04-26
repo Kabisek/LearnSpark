847b40d76675db4cc0ad108021c87410324ace36
Oshadhi Vibodha <153888394+OshiiV@users.noreply.github.com>
2025-04-26 21:11:35 +0530

    coded entity file

--- comment/src/main/java/com/example/comment/entity/Comment.java
+++ comment/src/main/java/com/example/comment/entity/Comment.java
@@ -1,5 +1,68 @@
 package com.example.comment.entity;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "comments")
 public class Comment {
 
+    @Id
+    private String id;
+    private String content;
+    private String postId;
+    private String userId;
+    private LocalDateTime createdAt;
+
+    public Comment() {
+    }
+
+    public Comment(String id, String content, String postId, String userId, LocalDateTime createdAt) {
+        this.id = id;
+        this.content = content;
+        this.postId = postId;
+        this.userId = userId;
+        this.createdAt = createdAt;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
