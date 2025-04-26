111dee6011691d847ffc37701b82955e82af5668
Divanka Samarasinghe <119291780+divankase@users.noreply.github.com>
2025-04-26 20:18:00 +0530

    removing codebase due to initializing error

--- learnspark2/src/main/java/com/example/learnspark/repository/LearningPlanRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.learnspark.repository;
-
-import com.example.learnspark.entity.LearningPlan;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
-
-public interface LearningPlanRepository extends MongoRepository<LearningPlan, String> {
-    List<LearningPlan> findByUserId(String userId);
-}
