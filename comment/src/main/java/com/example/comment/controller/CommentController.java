73fc9191b0e4a8ecc19f2632c7912564238b2dfb
Oshadhi Vibodha <153888394+OshiiV@users.noreply.github.com>
2025-04-26 21:15:15 +0530

    finished controller file

--- comment/src/main/java/com/example/comment/controller/CommentController.java
+++ comment/src/main/java/com/example/comment/controller/CommentController.java
@@ -1,5 +1,42 @@
 package com.example.comment.controller;
 
+
+import com.example.comment.entity.Comment;
+import com.example.comment.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/comments")
 public class CommentController {
 
+    @Autowired
+    private CommentService commentService;
+
+    @PostMapping
+    public Comment createComment(@RequestBody Comment comment) {
+        return commentService.createComment(comment);
+    }
+
+    @GetMapping
+    public List<Comment> getAllComments() {
+        return commentService.getAllComments();
+    }
+
+    @GetMapping("/{id}")
+    public Comment getCommentById(@PathVariable String id) {
+        return commentService.getCommentById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Comment updateComment(@PathVariable String id, @RequestBody Comment comment) {
+        return commentService.updateComment(id, comment);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteComment(@PathVariable String id) {
+        commentService.deleteComment(id);
+    }
 }
