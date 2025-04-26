29eb115296aa0eb5630712cd96c4f38c33582e9c
Kabisek <kabiseksundar02@gmail.com>
2025-04-26 20:39:35 +0530

    finished the controller file

--- postmanagement/src/main/java/com/example/postmanagement/controller/PostController.java
+++ postmanagement/src/main/java/com/example/postmanagement/controller/PostController.java
@@ -1,5 +1,41 @@
 package com.example.postmanagement.controller;
 
+import com.example.postmanagement.entity.Post;
+import com.example.postmanagement.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/posts")
 public class PostController {
 
+    @Autowired
+    private PostService postService;
+
+    @PostMapping
+    public Post createPost(@RequestBody Post post) {
+        return postService.createPost(post);
+    }
+
+    @GetMapping
+    public List<Post> getAllPosts() {
+        return postService.getAllPosts();
+    }
+
+    @GetMapping("/{id}")
+    public Post getPostById(@PathVariable String id) {
+        return postService.getPostById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Post updatePost(@PathVariable String id, @RequestBody Post post) {
+        return postService.updatePost(id, post);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePost(@PathVariable String id) {
+        postService.deletePost(id);
+    }
 }
