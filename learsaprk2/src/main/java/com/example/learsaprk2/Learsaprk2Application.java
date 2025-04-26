9c5f959bed09e98c58890c349345f000ac765876
Divanka Samarasinghe <119291780+divankase@users.noreply.github.com>
2025-04-26 20:19:42 +0530

    Project creating (spring web + mongo db)

---
+++ learsaprk2/src/main/java/com/example/learsaprk2/Learsaprk2Application.java
@@ -0,0 +1,13 @@
+package com.example.learsaprk2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learsaprk2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Learsaprk2Application.class, args);
+	}
+
+}
---
+++ learsaprk2/src/test/java/com/example/learsaprk2/Learsaprk2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learsaprk2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Learsaprk2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
