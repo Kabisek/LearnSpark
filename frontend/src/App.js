142609171960230ce31ae090fa0597ba14e2b600
Kabisek <kabiseksundar02@gmail.com>
2025-05-07 00:27:53 +0530

    FInal commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,15 +6,15 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
-import AddPost from './pages/AddPost';
-import EditPost from './pages/EditPost';
+// import AddPost from './pages/AddPost';
+// import EditPost from './pages/EditPost';
 import Navbar from './components/Navbar';
 import LearningPlans from './pages/LearningPlans';
 import MyLearningPlans from './pages/MyLearningPlans';
 import AddLearningPlan from './pages/AddLearningPlan';
-import Settings from './pages/Settings';
+// import Settings from './pages/Settings';
 import OAuthSuccess from './pages/OAuthSuccess';
-import FollowRequests from './components/FollowRequests';
+// import FollowRequests from './components/FollowRequests';
 import './App.css'
 
 import { ToastContainer } from 'react-toastify';
@@ -85,14 +85,10 @@ export default function App() {
             <Route path="/signin" element={<SignIn />} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
-            <Route path="/add-post" element={<PrivateRoute><AddPost /></PrivateRoute>} />
-            <Route path="/edit-post/:id" element={<PrivateRoute><EditPost /></PrivateRoute>} />
             <Route path="/learning-plans" element={<LearningPlans />} />
             <Route path="/my-learning-plans" element={<MyLearningPlans />} />
             <Route path="/add-learning-plan" element={<AddLearningPlan />} />
-            <Route path="/settings" element={<PrivateRoute><Settings /></PrivateRoute>} />
             <Route path="/oauth-success" element={<OAuthSuccess />} />
-            <Route path="/follow-requests" element={<PrivateRoute><FollowRequests /></PrivateRoute>} />
           </Routes>
         </Router>
       </ThemeProvider>
