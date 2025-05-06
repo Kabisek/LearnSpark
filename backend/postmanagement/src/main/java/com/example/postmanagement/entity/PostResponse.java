e3290eaa3c8609a9b500c705a6ef5e0212b3c66b
Kabisek <kabiseksundar02@gmail.com>
2025-05-06 21:09:54 +0530

    edited Post response file

--- backend/postmanagement/src/main/java/com/example/postmanagement/entity/PostResponse.java
+++ backend/postmanagement/src/main/java/com/example/postmanagement/entity/PostResponse.java
@@ -2,4 +2,27 @@ package main.java.com.example.postmanagement.entity;
 
 public class PostResponse {
 
+    private Post post;
+    private User user;
+
+    public PostResponse(Post post, User user) {
+        this.post = post;
+        this.user = user;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
